7dc47989cddba932a21732d1385ae43057245b39
Przemysław Gawlas <prgawlas@gmail.com>
2019-05-01 10:53:17 +0200

    color

--- src/Game.config.js
+++ src/Game.config.js
@@ -14,7 +14,7 @@ export const BrickColor = (key) => {
 	switch (key) {
 		case 1: return 0xFF0000;
 		case 2: return 0x00FF00;
-		case 3: return 0x0000FF;
+		case 3: return 0x0080ff;
 		case 4: return 0xFFFF00;
 		case 5: return 0x00FFFF;
 		case 6: return 0xFF00FF;
